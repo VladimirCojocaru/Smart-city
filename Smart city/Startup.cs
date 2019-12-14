a0a2c2c78d2762eed856e2d05c1fe874972db342
Vladimir Cojocaru <cojocaruvladimir@yahoo.com>
2019-12-14 16:56:52 +0200

    Commit1

---
+++ Smart city/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Smart_city.Startup))]
+namespace Smart_city
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
